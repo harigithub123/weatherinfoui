8edcae4b881e166f3d0f459535391ce727070d3d
Hari Nale <harinale132@gmail.com>
2020-11-14 13:18:06 +0530

    added changes to handle the error.

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { timer } from 'rxjs';
+import { throwError, timer } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Hour } from './Hour.model';
 import { map } from 'rxjs/operators';
@@ -22,7 +22,7 @@ export class WeatherComponent implements OnInit {
   }
 
   getWeatherInfo() {
-    this.http.get<Hour>('/weather/info/get?zip='+this.zipCode+'&days='+this.days)
+    this.http.get<Hour>('/weather-app/forecast/temperature?zip='+this.zipCode+'&days='+this.days)
     .pipe(
       map(responseData => {
         const hours: Hour[] = [];
@@ -37,8 +37,24 @@ export class WeatherComponent implements OnInit {
     .subscribe(hours => {
       console.log(hours);
       this.forecastHours = hours;
+    },
+    (err) => {
+      this.forecastHours = [];
+      this.handleError(err);
     });
   }
 
+  handleError(error) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // client-side error
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
 
 }
